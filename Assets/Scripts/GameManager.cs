da64d3b5197d9b625f1571dd8f55315b79e4cf27
James Lin <james97lin@gmail.com>
2017-12-19 01:58:02 -0800

    Added health and level progression logic. Added falling via gravity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -11,21 +12,27 @@ public class GameManager : MonoBehaviour {
     public Vector3 cameraOffsetPos;
     public Vector3 cameraRotation;
     [Range(0, 5)]
-    public float minTweenDuration, maxTweenDuration;
+    public float minTweenDelay, maxTweenDelay;
     [Range(0, 5)]
-    public float playerTweenDuration;
-    [Range(0, 10)]
+    public float tweenDuration;
+    [Range(0, 30)]
     public float tweenDistance;
+    public int startingHealth;
 
     [Header("Variables")]
     public GameObject cam;
     public GameObject playerPrefab;
+    public GameObject canvas;
 
     LevelManager levelManager;
     GameObject player;
     int currentDepth;
     Level currentLevel;
+    GameObject currentLevelObj;
     int buttonsRemaining;
+    int currentHealth;
+    Text healthText;
+    Text depthText;
 
     void Awake() {
         if (instance == null)
@@ -41,11 +48,24 @@ public class GameManager : MonoBehaviour {
     void Start() {
         // Initialize.
         currentDepth = 0;
+        currentHealth = startingHealth;
         levelManager = LevelManager.instance;
+        healthText = canvas.transform.Find("HealthText").GetComponent<Text>();
+        depthText = canvas.transform.Find("DepthText").GetComponent<Text>();
+
+        // Update canvas.
+        UpdateCanvas();
 
         // Kick-start game.
         currentLevel = ChooseLevel();
         SetupLevel(currentLevel);
+    }
+
+    /** Updates the canvas with proper values of health and depth.
+     */
+    void UpdateCanvas() {
+        healthText.text = "Health: " + currentHealth;
+        depthText.text = "Depth: " + currentDepth;
     }
 
     #region Level Setup/Cleanup
@@ -53,7 +73,7 @@ public class GameManager : MonoBehaviour {
     /** Selects the next level to load and returns it.
      */
     private Level ChooseLevel() {
-        return levelManager.levels[0];
+        return levelManager.levels[1];
     }
 
     /** Given a level, loads it and spawns in the player.
@@ -73,42 +93,48 @@ public class GameManager : MonoBehaviour {
         cam.transform.eulerAngles = cameraRotation;
 
         // Load in the level.
-        GameObject levelObj = Instantiate(level.levelPrefab, Vector3.zero, Quaternion.identity);
+        Vector3 diffVec = new Vector3(0, tweenDistance, 0);
+        currentLevelObj = Instantiate(level.levelPrefab, diffVec, Quaternion.identity);
 
         // Tween the environment down.
-        Vector3 diffVec = new Vector3(0, tweenDistance, 0);
-        foreach (Transform child in levelObj.transform.Find("Floor")) {
-            float duration = Random.Range(minTweenDuration, maxTweenDuration);
-            iTween.FadeFrom(child.gameObject, 0, duration);
-            iTween.MoveFrom(child.gameObject, iTween.Hash("position", child.position + diffVec,
-                                                          "time", duration,
-                                                          "easetype", "easeOutQuart"));
+        foreach (Transform child in currentLevelObj.transform.Find("Floor")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeFrom(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", -diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
         }
-        foreach (Transform child in levelObj.transform.Find("Walls")) {
-            float duration = Random.Range(minTweenDuration, maxTweenDuration);
-            iTween.FadeFrom(child.gameObject, 0, duration);
-            iTween.MoveFrom(child.gameObject, iTween.Hash("position", child.position + diffVec,
-                                                          "time", duration,
-                                                          "easetype", "easeOutQuart"));
+        foreach (Transform child in currentLevelObj.transform.Find("Walls")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeFrom(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", -diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
         }
-        foreach (Transform child in levelObj.transform.Find("Buttons")) {
-            float duration = Random.Range(minTweenDuration, maxTweenDuration);
-            iTween.FadeFrom(child.gameObject, 0, duration);
-            iTween.MoveFrom(child.gameObject, iTween.Hash("position", child.position + diffVec,
-                                                          "time", duration,
-                                                          "easetype", "easeOutQuart"));
+        yield return new WaitForSeconds(tweenDuration + maxTweenDelay);
+
+        // Tween the interactables down.
+        foreach (Transform child in currentLevelObj.transform.Find("Buttons")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeFrom(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", -diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
         }
-        yield return new WaitForSeconds(maxTweenDuration + .5f);
+        yield return new WaitForSeconds(tweenDuration + maxTweenDelay);
 
         // Spawn in the player, tween it down, enable movement.
         player = Instantiate(playerPrefab, playerPosition, Quaternion.identity);
         player.GetComponent<PlayerController>().LinkCamera(cam);
-        iTween.FadeFrom(player, 0, playerTweenDuration);
+        iTween.FadeFrom(player, 0, tweenDuration);
         iTween.MoveFrom(player, iTween.Hash("position", player.transform.position + diffVec,
-                                                          "time", playerTweenDuration,
-                                                          "easetype", "easeOutQuart"));
-        yield return new WaitForSeconds(playerTweenDuration + .5f);
-        player.GetComponent<PlayerController>().moving = false;
+                                            "time", tweenDuration,
+                                            "easetype", "easeOutQuart"));
+        yield return new WaitForSeconds(tweenDuration + .5f);
+        player.GetComponent<PlayerController>().canMove = true;
     }
 
     #endregion
@@ -119,7 +145,56 @@ public class GameManager : MonoBehaviour {
      *  Cleans up the current level and loads a new one.
      */
     public void LevelFinished() {
-        print("You win!");
+        StartCoroutine(LevelFinishedCoroutine());
+    }
+
+    IEnumerator LevelFinishedCoroutine() {
+        // Logic
+        currentDepth += 1;
+        UpdateCanvas();
+
+        // Tween player up, remove player.
+        Vector3 diffVec = new Vector3(0, tweenDistance, 0);
+        //iTween.FadeTo(player, 0, tweenDuration);
+        iTween.MoveBy(player, iTween.Hash("amount", diffVec,
+                                          "time", tweenDuration,
+                                          "easetype", "easeInQuart"));
+        yield return new WaitForSeconds(tweenDuration);
+        Destroy(player);
+
+        // Tween environment up, remove environment.
+        foreach (Transform child in currentLevelObj.transform.Find("Buttons")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeTo(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
+        }
+        yield return new WaitForSeconds(tweenDuration + maxTweenDelay);
+
+        foreach (Transform child in currentLevelObj.transform.Find("Floor")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeTo(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
+        }
+        foreach (Transform child in currentLevelObj.transform.Find("Walls")) {
+            float delay = Random.Range(minTweenDelay, maxTweenDelay);
+            iTween.FadeTo(child.gameObject, 0, delay + tweenDuration);
+            iTween.MoveAdd(child.gameObject, iTween.Hash("amount", diffVec,
+                                                         "time", tweenDuration,
+                                                         "easetype", "easeOutQuart",
+                                                         "delay", delay));
+        }
+        yield return new WaitForSeconds(tweenDuration + maxTweenDelay);
+        Destroy(currentLevelObj);
+
+        // Prepare next level.
+        currentLevel = ChooseLevel();
+        SetupLevel(currentLevel);
     }
 
     /** A button was activated.
@@ -127,6 +202,7 @@ public class GameManager : MonoBehaviour {
     public void ButtonActivated() {
         buttonsRemaining -= 1;
         if (buttonsRemaining == 0) {
+            player.GetComponent<PlayerController>().canMove = false;
             LevelFinished();
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,47 +9,49 @@ public class PlayerController : MonoBehaviour {
     public float speed; // Speed of the player in units per second.
 
     [Header("Logic")]
-    public bool moving = true; // Is the player currently moving?
+    public bool canMove = false;
+    public bool moving = false; // Is the player currently moving?
 
     GameObject cam;
     Vector3 relCamPos; // Camera position relative to player.
     GameObject playerCube;
 
-    ObstacleDetector upDetector, downDetector, leftDetector, rightDetector;
-
     void Awake() {
         playerCube = transform.Find("PlayerCube").gameObject;
-        upDetector = transform.Find("Colliders").Find("Up").GetComponent<ObstacleDetector>();
-        downDetector = transform.Find("Colliders").Find("Down").GetComponent<ObstacleDetector>();
-        leftDetector = transform.Find("Colliders").Find("Left").GetComponent<ObstacleDetector>();
-        rightDetector = transform.Find("Colliders").Find("Right").GetComponent<ObstacleDetector>();
     }
 
-    // Update is called once per frame
     void Update () {
+        ManageGravity();
         ManageInput();
         ManageCamera();
 	}
 
+    /** Link the camera to the player's movement.
+     */
     public void LinkCamera(GameObject cam) {
         this.cam = cam;
         relCamPos = cam.transform.position - playerCube.transform.position;
     }
 
+    /** Input manager for the player.
+     */
     void ManageInput()
     {
-        if (moving) return;
+        if (moving || !canMove) return;
 
-        if (Input.GetKey("up") && !upDetector.colliding) {
+        if ((Input.GetKey("up") || Input.GetKey(KeyCode.W)) && IsDirectionClear("up")) {
             moving = true;
             StartCoroutine(Move("up"));
-        } else if (Input.GetKey("down") && !downDetector.colliding) {
+        }
+        else if ((Input.GetKey("down") || Input.GetKey(KeyCode.S)) && IsDirectionClear("down")) {
             moving = true;
             StartCoroutine(Move("down"));
-        } else if (Input.GetKey("left") && !leftDetector.colliding) {
+        }
+        else if ((Input.GetKey("left") || Input.GetKey(KeyCode.A)) && IsDirectionClear("left")) {
             moving = true;
             StartCoroutine(Move("left"));
-        } else if (Input.GetKey("right") && !rightDetector.colliding) {
+        }
+        else if ((Input.GetKey("right") || Input.GetKey(KeyCode.D)) && IsDirectionClear("right")) {
             moving = true;
             StartCoroutine(Move("right"));
         }
@@ -102,9 +104,42 @@ public class PlayerController : MonoBehaviour {
         moving = false;
     }
 
+    /** Check if a given direction is clear (true) or not (false).
+     */
+    bool IsDirectionClear(string dir) {
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.zero;
+        switch (dir) {
+            case "up":
+                direction = new Vector3(0, 0, 1);
+                break;
+            case "down":
+                direction = new Vector3(0, 0, -1);
+                break;
+            case "right":
+                direction = new Vector3(1, 0, 0);
+                break;
+            case "left":
+                direction = new Vector3(-1, 0, 0);
+                break;
+            case "below":
+                direction = new Vector3(0, -1, 0);
+                break;
+        }
+        return !Physics.Raycast(origin, direction, 1);
+    }
+
     void ManageCamera()
     {
         Vector3 vec = playerCube.transform.position + relCamPos;
         cam.transform.position = new Vector3(vec.x, cam.transform.position.y, vec.z);
+    }
+
+    void ManageGravity() {
+        if (canMove && !moving && IsDirectionClear("below")) {
+            canMove = false;
+            playerCube.GetComponent<Rigidbody>().useGravity = true;
+            playerCube.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
